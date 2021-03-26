c69bb64a1b6a82256a38577b3123a7500d4f3383
Scott Obara <scott@rubbscreations.com>
2021-03-26 13:44:11 +1100

    Unsaved changes

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
             .padding()
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .border(Color.red, width: 2)
+            .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .strokeBorder(Color.blue, lineWidth: 3)
+                )
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct ContentView: View {
+
+    var body: some View {
+        Text(".titleStyle()")
+            .titleStyle()
     }
 }
 
@@ -19,3 +43,13 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+/*
+ Day 24 of #100DaysOfSwiftUI: Project 3, part two
+
+ Curious to find that outline() does not get applied to clipShape(). Found the following recommending the use of overlay() for applying outlines to shapes
+
+ https://www.hackingwithswift.com/quick-start/swiftui/how-to-draw-a-border-around-a-view
+
+ */
